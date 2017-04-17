08bafbe1d117cfeaabdc5a2164d8bfc2a38e3717
Samson Kapeyi <skapeyi@bitbucket.org>
2017-04-17 23:04:22 +0300

    Updated readme and set up thirdparty plugins

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'DrugDash'),
+    'name' => "DrugDash",
 
     /*
     |--------------------------------------------------------------------------
@@ -177,6 +177,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * Third party providers
+        */
+        Collective\Html\HtmlServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
+        Zizaco\Entrust\EntrustServiceProvider::class,
     ],
 
     /*
@@ -225,6 +231,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //Third party aliases
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,14 @@
                 </div>
             </div>
         </nav>
+        <div class="container">
+          @if (session()->has('flash_notification.message'))
+            <div class="alert alert-{{ session('flash_notification.level') }}">
+              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                {!! session('flash_notification.message') !!}
+            </div>
+          @endif
+        </div>
 
         @yield('content')
     </div>
