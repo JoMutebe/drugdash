dd4a9ce34054b2bf448e1cecee357d673c9cd4ed
Samson Kapeyi <skapeyi@github.com>
2017-04-18 10:57:24 +0300

    Created migrations, models and resource controllers for system entities

--- database/migrations/2017_04_18_074029_create_counties_table.php
+++ database/migrations/2017_04_18_074029_create_counties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCountiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('counties', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone',12)->unique();
-            $table->string('access_token',128)->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('counties');
     }
 }
