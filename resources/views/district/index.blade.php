3fd6cc001232760da3fea8bff6a4a227b0fea1fd
Samson Kapeyi <skapeyi@github.com>
2017-04-20 17:39:01 +0300

    Added template files. Some sections not working because of loading two versions of jquery

--- resources/views/district/index.blade.php
+++ resources/views/district/index.blade.php
@@ -31,7 +31,7 @@
 
   <script>
   $(document).ready(function() {
-    $.noConflict();
+    // $.noConflict();
     $('#districts').DataTable({
         processing: true,
         serverSide: true,
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,22 +4,27 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Datatables -->
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap-->
+    <link href="{{ asset('css/bootstrap/bootstrap.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome -->
     <link href="{{ asset('css/fa/css/font-awesome.min.css') }}" rel="stylesheet">
 
+    <!-- Gentellela -->
+    <link href="{{ asset('css/gentelella.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery.js"></script>
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 
     <script>
@@ -28,76 +33,180 @@
         ]) !!};
     </script>
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+<body class="nav-md">
+  <div class="container body">
+    <div class="main_container">
+      <div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+          <div class="navbar nav_title" style="border: 0;">
+            <a href="/" class="site_title"><i class="fa fa-paw"></i> <span>{{ config('app.name', 'Laravel') }}</span></a>
+          </div>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+          <div class="clearfix"></div>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+          <!-- menu profile quick info -->
+          <div class="profile">
+            <div class="profile_pic">
+              <img src="img/user.png" alt="..." class="img-circle profile_img">
             </div>
-        </nav>
-        <div class="container">
-          @if (session()->has('flash_notification.message'))
-            <div class="alert alert-{{ session('flash_notification.level') }}">
-              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                {!! session('flash_notification.message') !!}
+            <div class="profile_info">
+              <span>Welcome,</span>
+              <h2>{{ Auth::user()->name }}</h2>
             </div>
-          @endif
+          </div>
+          <!-- /menu profile quick info -->
+
+          <br />
+
+          <!-- sidebar menu -->
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+
+            <div class="menu_section">
+              <h3> General</h3>
+              <ul class="nav side-menu">
+                <li><a><i class="fa fa-home"></i> Location <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li><a href="/districts">Districts</a></li>
+                    <li><a href="/counties">Counties</a></li>
+                    <li><a href="/subcounties">Subcounties</a></li>
+                    <li><a href="/parishes">Parishes</a></li>
+                    <li><a href="/villages">Villages</a></li>
+                  </ul>
+                </li>
+            </div>
+          </div>
+          <!-- /sidebar menu -->
+
+          <!-- /menu footer buttons -->
+          <div class="sidebar-footer hidden-small">
+            <a data-toggle="tooltip" data-placement="top" title="Settings">
+              <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+              <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Lock">
+              <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+            </a>
+
+          </div>
+          <!-- /menu footer buttons -->
         </div>
+      </div>
 
+      <!-- top navigation -->
+      <div class="top_nav">
+        <div class="nav_menu">
+          <nav class="" role="navigation">
+            <div class="nav toggle">
+              <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li class="">
+                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                  <img src="img/user.png">{{ Auth::user()->name }}
+                  <span class=" fa fa-angle-down"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-usermenu pull-right">
+                  <li><a href="javascript:;"> Profile</a></li>
+                  <li>
+                    <a href="javascript:;">
+                      <!-- <span class="badge bg-red pull-right">50%</span> -->
+                      <span>Settings</span>
+                    </a>
+                  </li>
+                  <li><a href="javascript:;">Help</a></li>
+                  <li><a href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+                      Logout
+                  </a></li>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
+                </ul>
+              </li>
+
+              <li role="presentation" class="dropdown">
+                <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false" title="Notifications">
+                  <i class="fa fa-bell"></i>
+                  <!-- <span class="badge bg-green">6</span> -->
+                </a>
+                <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                  <li>
+                    <a>
+                      <span class="image"><img src="img/user.png" alt="Profile Image" /></span>
+                      <span>
+                        <span>DHO</span>
+                        <span class="time">2 mins ago</span>
+                      </span>
+                      <span class="message">
+                        We have new supplies for HIV drugs.
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span class="image"><img src="img/user.png" alt="Profile Image" /></span>
+                      <span>
+                        <span>RDC</span>
+                        <span class="time">2 hours ago</span>
+                      </span>
+                      <span class="message">
+                        Do you guys want the budget to be improved?
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a>
+                      <span class="image"><img src="img/user.png" alt="Profile Image" /></span>
+                      <span>
+                        <span>In charge</span>
+                        <span class="time">3 days ago</span>
+                      </span>
+                      <span class="message">
+                        We received our supplies for this cycle.
+                      </span>
+                    </a>
+                  </li>
+
+                  <li>
+                    <div class="text-center">
+                      <a>
+                        <strong>See All Alerts</strong>
+                        <i class="fa fa-angle-right"></i>
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <!-- /top navigation -->
+
+      <!-- page content -->
+      <div class="right_col" role="main">
         @yield('content')
-    </div>
+      </div>
+      <!-- /page content -->
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+      <!-- footer content -->
+      <footer>
+        <div class="pull-right">
+          DrugDash - A drug decision support system by  <a href="http://prunestech.com" target="_blank">Prunes Technologies</a>
+        </div>
+        <div class="clearfix"></div>
+      </footer>
+      <!-- /footer content -->
+    </div>
+  </div>
+  <script src="{{ asset('js/app.js')}}"></script>
+  <script src="{{ asset('js/bootstrap.js')}}"></script>
+
+  <!-- Custom Theme Script -->
+  <script src="{{ asset('js/gentelella.js')}}"></script>
 </body>
 </html>
