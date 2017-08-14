81b127bd8290211cbf5d067a1b3084268b672b79
skahuma <skahuma@github.com>
2017-08-14 15:32:06 +0300

    data visualization graphs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -138,7 +138,12 @@
 <div class="row">
   <div class="col-md-12"  id="graph"  style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 </div>
-
+<div class="row">
+  <div class="col-md-12"  id="container"  style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+</div>
+<div class="row">
+  <div class="col-md-12"  id="data"  style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+</div>
 @endsection
 
 @push('scripts')
@@ -177,9 +182,96 @@
         data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
     },
     {
-        name: 'HC 4',
+        name: 'Bukedea HC 4',
         data: [2.0, 9.9, 11.5, 34.5, 18.4, 21.5, 14.2, 6.5, 3.3, 1.3, 0.9, 0.6]
     },]
 });
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Average Monthly StockOut Tallies'
+    },
+   xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'Kabarwa HC',
+        data: [-3.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+    }, {
+        name: 'Malerwa  HC',
+        data: [3.9, 4.2, -4.0, 8.5, 11.9, 15.2, 17.0, -2.0, 14.2, 10.3, -6.6, 4.8]
+    },
+    {
+        name: 'Bukedea HC 4',
+        data: [-2.0, 9.9, 11.5, 34.5, -3.0, 21.5, 14.2, 6.5, 3.3, -1.3, 0.9, 0.6]
+    },]
+});
+Highcharts.chart('data', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Average Monthly Stock Usage Comparisons'
+    },
+    subtitle: {
+        text: ''
+    },
+    xAxis: {
+        categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Unit of measurement ( Boxes)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} boxes</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Kabarwa HC',
+        data: [49.9, 71.5, 40.4, 30.2, 44.0, 50.0, 90.6, 20.5, 90.4, 30.1, 95.6, 20.4]
+
+    }, {
+        name: 'Malerwa  HC',
+        data: [83.6, 78.8, 98.5, 93.4, 6.0, 84.5, 5.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+    }, {
+        name: 'Bukedea HC 4',
+        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+    }]
+});
 </script>
+
 @endpush
