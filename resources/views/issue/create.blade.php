6caa3e34d24c580b57fa327753aee2b02bc651f1
Samson Kapeyi <skapeyi@github.com>
2017-08-18 16:33:39 +0300

    Re-added view

---
+++ resources/views/issue/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <h3>Add New Issue</h3>
+      {!! Form::open(['action' => 'IssueController@store']) !!}
+
+    <div class="form-group">
+          {!! Form::label('district_id', 'District') !!}
+          <select class="form-control" name="district_id" id="district_id" data-parsley-required="true">
+          @foreach ($districts as $district)
+          {
+            <option value="{{ $district->id }}">{{ $district->name }}</option>
+          }
+          @endforeach
+        </select>
+      </div>
+      <div class="form-group">
+          {!! Form::label('healthfacility_id', 'Healthfacility ID') !!}
+          <select class="form-control" name="healthfacility_id" id="healthfacility_id" data-parsley-required="true">
+          @foreach ($healthfacilities as $healthfacility)
+          {
+            <option value="{{ $healthfacility->id }}">{{ $healthfacility->name }}</option>
+          }
+          @endforeach
+        </select>
+      </div>
+      
+      <div class="form-group">
+          {!! Form::label('description', 'Description') !!}
+          {!! Form::textarea('description', '',['class' => 'form-control']) !!}
+      </div>
+
+
+
+      <div class="form-group">
+          <button type="submit" class="btn btn-primary">
+              Submit
+          </button>
+      </div>
+      {!! Form::close() !!}
+  </div>
+@endsection
